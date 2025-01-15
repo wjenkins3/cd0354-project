363cc63d65d69b63a016008b3f1b3b2b0cbcec63
Alex Jenkins <wjenkins3@users.noreply.github.com>
2025-01-15 14:00:48 -0500

    Change API host to External IP

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a2c0d62d2e2b24391a13ec1cd16138d8-290388196.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
